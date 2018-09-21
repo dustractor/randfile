02619c560596ff955e13ac93c87776f66bc9ed6e
Shams Kitz <dustractor@gmail.com>
2018-09-21 01:27:21 -0500

    some-work

--- randfile.py
+++ randfile.py
@@ -1,152 +1,175 @@
+# randfile.py
+from pathlib import Path
+from sys import exit
+from os import getenv
+from shutil import copy
+from os.path import expanduser
+from tkinter import Tk,StringVar,filedialog
+from tkinter.ttk import Entry,Label,Button,Frame,LabelFrame,OptionMenu
+from sqlite3 import connect,Connection
+from argparse import ArgumentParser
+from ttkthemes import ThemedStyle
 
-import os
-import pathlib
-import shutil
-import sqlite3
-from tkinter import (
-        Tk,
-        Frame,
-        Entry,
-        Label,
-        Listbox,
-        Button,
-        StringVar,
-        filedialog)
-from tkinter.ttk import LabelFrame
+class Cx(object):
+    def __init__(self,cx):
+        self.cx = cx
+
+class Data(Cx):
+    def set(self,attr,value):
+        print("setting",attr,"to",value)
+        self.cx.execute(
+                "insert into data(name,value)"
+                "values (?,?)",
+                (attr,value))
+        self.cx.commit()
+    def get(self,attr):
+        return self.cx.cu.execute(
+            "select value from data where name=?", (attr,)).fetchone()
+
+class Files(Cx):
+    def add(self,path):
+        self.cx.execute("insert into fscache(path) values (?)",(path,))
+    def pick(self):
+        return self.cx.cu.execute(
+                "select path from fscache order by random() limit 1"
+                ).fetchone()
 
 
-class Db(sqlite3.Connection):
+
+class AppData(Connection):
+    definitions = """
+
+            create table if not exists data (
+            name, value default '',
+            unique(name) on conflict replace);
+
+            create table if not exists fscache (
+            path,unique (path) on conflict ignore);
+
+            """
+
     def __init__(self,*kw,**kwds):
-        super().__init__(*kw,*kwds)
-        self.executescript( """
-                create table if not exists settings( name text, value text,
-                unique (name) on conflict replace);
-                create table if not exists files( path text,
-                unique (path) on conflict replace); """)
+        super().__init__(*kw,**kwds)
+        self.executescript(self.definitions)
         self.commit()
         self.cu = self.cursor()
-        self.cu.row_factory=lambda c,r:r[0]
+        self.cu.row_factory = lambda c,r:r[0]
+        self.data = Data(self)
+        self.files = Files(self)
 
-dbfile = os.path.expanduser("~/_randfile.db")
-db = Db(dbfile)
+args = ArgumentParser()
+args.add_argument("--get",action="store_true")
+args.add_argument("--clear",action="store_true")
+args.add_argument("--theme",default="equilux")
+argument = args.parse_args()
 
+data_dir = Path(str(getenv("LOCALAPPDATA"))) / "randfile"
+data_dir.mkdir(exist_ok=True)
+db_path = str(data_dir / "randfile_data.sqlitedb")
+db = connect(db_path,factory=AppData)
+data = db.data
+files = db.files
 
-class App(Tk):
-    def __init__(self,master=None):
-        super().__init__(master)
-        self.input_dir = StringVar()
-        self.filter_ext = StringVar()
-        self.output_dir = StringVar()
-        self.input_dir.trace('w',self.input_dir_trace)
-        self.output_dir.trace('w',self.output_dir_trace)
-        self.filter_ext.trace('w',self.filter_ext_trace)
-        input_dir = db.cu.execute(
-                "select value from settings where name=?",
-                ("input_dir",)).fetchone()
-        if input_dir:
-            self.input_dir.set(input_dir)
-        output_dir = db.cu.execute(
-                "select value from settings where name=?",
-                ("output_dir",)).fetchone()
-        if output_dir:
-            self.output_dir.set(output_dir)
-        filter_ext = db.cu.execute(
-                "select value from settings where name=?",
-                ("filter_ext",)).fetchone()
-        if filter_ext:
-            self.filter_ext.set(filter_ext)
-        else:
-            self.filter_ext.set("mp3")
-
-        self.lbl_input_dir = Label(self,textvariable=self.input_dir)
-        self.frame_input = LabelFrame(self,labelwidget=self.lbl_input_dir)
-        self.frame_input.pack(fill="both",expand=True)
-
-        self.btn_input_dir_select = Button(self.frame_input,text="Select Input Dir",
-                command=self.ask_inputdir_cmd)
-        self.btn_input_dir_select.pack()
-
-        self.lbl_filter_ext = Entry(self.frame_input,textvariable=self.filter_ext)
-        self.lbl_filter_ext.pack()
-        self.btn_input_dir_scan = Button(self.frame_input,text="Scan Input Dir",
-                command=self.scan_inputdir_cmd)
-        self.btn_input_dir_scan.pack()
-
-        self.lbl_output_dir = Label(self,textvariable=self.output_dir)
-        self.frame_output = LabelFrame(self,labelwidget=self.lbl_output_dir)
-        self.frame_output.pack(fill="both",expand=True)
-
-        self.btn_output_dir_select = Button(self.frame_output,text="Select Output Dir",
-                command=self.ask_outputdir_cmd)
-        self.btn_output_dir_select.pack()
-        self.btn_get_random_file = Button(self.frame_output,text="Get Random File",
-                command=self.get_random_file_cmd)
-        self.btn_get_random_file.pack()
-        self.bind("<Control-q>",lambda *_:self.quit())
-        self.bind("<Control-w>",lambda *_:self.quit())
-
-    def scan_inputdir_cmd(self):
-        path = self.input_dir.get()
-        ext = "." + self.filter_ext.get()
-        if not pathlib.Path(path).is_dir():
-            return
-        try:
-            for r,ds,fs in os.walk(path):
-                for f in fs:
-                    if f.endswith(ext):
-                        path = os.path.join(r,f)
-                        db.execute(
-                                "insert into files(path) values (?)",
-                                (path,))
-        except:
-            pass
-        finally:
-            db.commit()
-
-    def input_dir_trace(self,*v):
-        t = pathlib.Path(self.input_dir.get())
-        if t.is_dir():
-            db.execute(
-                    "insert into settings(name,value) values(?,?)",
-                    ("input_dir",str(t)))
-            db.commit()
-
-    def output_dir_trace(self,*v):
-        t = pathlib.Path(self.output_dir.get())
-        if t.is_dir():
-            db.execute(
-                    "insert into settings(name,value) values(?,?)",
-                    ("output_dir",str(t)))
-            db.commit()
-
-    def filter_ext_trace(self,*v):
-        t = self.filter_ext.get()
-        db.execute(
-                "insert into settings(name,value) values(?,?)",
-                ("filter_ext",t))
-        db.commit()
-    def ask_inputdir_cmd(self):
-        chosen_input_dir = pathlib.Path(filedialog.askdirectory())
-        if chosen_input_dir.is_dir():
-            print("you chose",chosen_input_dir)
-            self.input_dir.set(str(chosen_input_dir))
-
-    def ask_outputdir_cmd(self):
-        chosen_output_dir = pathlib.Path(filedialog.askdirectory())
-        if chosen_output_dir.is_dir():
-            print("you chose",chosen_output_dir)
-            self.output_dir.set(str(chosen_output_dir))
-
-    def get_random_file_cmd(self):
-        input_dir = self.input_dir.get()
-        output_dir = self.output_dir.get()
-        if ( pathlib.Path(input_dir).is_dir() and
-             pathlib.Path(output_dir).is_dir()):
-            rf = db.cu.execute("select path from files order by random() limit 1").fetchone()
-        shutil.copy(rf,output_dir)
-        print("random file (",rf,") copied to:", output_dir)
+if argument.clear:
+    db.execute("delete from fscache")
+    data.set("last_scan","")
+    exit()
 
 
-if __name__ == "__main__":
-    App().mainloop()
+if argument.get:
+    input_path,output_path,last_scan,extension_filter = map(data.get,
+            ("input_path","output_path","last_scan","extension_filter"))
+    if not input_path and output_path:
+        exit()
+    source = Path(input_path)
+    destination = Path(output_path)
+    if not source.is_dir() and destination.is_dir():
+        exit()
+    if not last_scan == input_path:
+        db.execute("delete from fscache")
+        print("Scanning",input_path)
+        globfilt = "**/*." + [extension_filter,"*"][extension_filter==None]
+        for i,f in enumerate(source.glob(globfilt)):
+            try:
+                files.add(str(f))
+            except UnicodeEncodeError:
+                pass
+        print(i,"files added")
+        data.set("last_scan",input_path)
+    selected_file = files.pick()
+    if not selected_file:
+        exit()
+    copy(selected_file,output_path)
+    print("Copied",selected_file,"to",output_path)
+    exit()
 
+
+def CxVar(name):
+    v = StringVar()
+    t = data.get(name)
+    if t:
+        v.set(t)
+    def trace(*_):
+        data.set(name,v.get())
+    v.trace("w",trace)
+    return v
+
+
+class App(Frame):
+    def __init__(self):
+        super().__init__()
+
+        self.v_lastscan = CxVar("last_scan")
+        self.v_extfilt = CxVar("extension_filter")
+        self.v_inputpath = CxVar("input_path")
+        self.v_outputpath = CxVar("output_path")
+
+        self.frame_top,self.frame_bot = Frame(self),Frame(self)
+
+        self.frame_input = LabelFrame(self.frame_top,text="Source")
+        self.frame_ext = LabelFrame(self.frame_top,text="Filetype Filter")
+        self.frame_output = LabelFrame(self.frame_bot,text="Destination")
+
+
+
+        self.btn_inputpath = Button(
+                self.frame_input,
+                textvariable=self.v_inputpath,
+                command=lambda:self.v_inputpath.set(filedialog.askdirectory(title="Choose input folder")))
+        
+        common_extensions = "mp3","ogg","wav","jpg","gif","png","blend"
+
+        self.opt_extfilt = OptionMenu(
+                self.frame_ext,
+                self.v_extfilt,
+                "mp3",
+                *common_extensions)
+
+        self.btn_outputpath = Button(
+                self.frame_output,
+                textvariable=self.v_outputpath,
+                command=lambda:self.v_outputpath.set(filedialog.askdirectory(title="Choose output folder")))
+        self.config(padding="10")
+
+        self.pack(fill="both",expand=True)
+
+        self.frame_top.pack(fill="both",expand=True)
+        self.frame_bot.pack(fill="both",expand=True)
+
+
+        self.frame_input.pack(side="left",fill="both",expand=True)
+        self.frame_ext.pack(fill="both",expand=True)
+
+        self.frame_output.pack(fill="x")
+
+        self.btn_inputpath.pack(anchor="ne")
+        self.opt_extfilt.pack(anchor="ne")
+        self.btn_outputpath.pack(anchor="ne")
+
+
+
+app = App()
+app.style = ThemedStyle()
+print("app.style.theme_names():",app.style.theme_names())
+app.style.set_theme(argument.theme)
+app.mainloop()
